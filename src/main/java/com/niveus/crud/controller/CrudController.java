c7849c77456a8c322e8be10effe39008e682bf40
prathameshshintre <pshintre29@gmail.com>
2022-11-08 16:13:46 +0530

    curd operations

--- src/main/java/com/niveus/crud/controller/CrudController.java
+++ src/main/java/com/niveus/crud/controller/CrudController.java
@@ -1,15 +1,33 @@
 package com.niveus.crud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.niveus.crud.entity.EmpEntity;
+import com.niveus.crud.entity.EmpEntityReponse;
+import com.niveus.crud.repository.EmpRepository;
+
 @RestController
+@RequestMapping(value= "/crud")
 public class CrudController {
 	
-	@PostMapping(value= "/getDetails")
-	public String getDetails()
+	@Autowired
+	EmpRepository repo;
+	@PostMapping(value= "/insert")
+	public ResponseEntity<EmpEntityReponse> enterData(@RequestBody EmpEntity request)
 	{
-		return "Welcome to niveus solutions";
+		EmpEntityReponse response=new EmpEntityReponse();
+		System.out.println(request.getEmpName()+ request.getEmpDesignation()+ request.getEmpDepartment() + request.getEmpEmailId());
+		this.repo.save(request);
+		response.setMessage("Record inserted");
+		
+		return new ResponseEntity<EmpEntityReponse>(response,HttpStatus.OK);
+		
 	}
 
 }
---
+++ src/main/java/com/niveus/crud/repository/EmpRepository.java
@@ -0,0 +1,11 @@
+package com.niveus.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.niveus.crud.entity.EmpEntity;
+
+@Repository
+public interface EmpRepository extends JpaRepository<EmpEntity, String>{
+	
+
+}
